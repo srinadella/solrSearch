b2a7b7926a280b3044a2c5e3897e8adec1cad5e9
cnadella@gmail.com <cnadella@gmail.com>
2016-03-05 21:06:58 -0500

    Update APIs

--- src/main/java/net/search/solr/services/SolrSearchAddAPI.java
+++ src/main/java/net/search/solr/services/SolrSearchAddAPI.java
@@ -1,9 +1,11 @@
 package net.search.solr.services;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,16 +13,61 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
-import org.apache.solr.client.solrj.SolrServerException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import net.search.solr.db.DBAccessor;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 @Path("/solrAddApi")
 public class SolrSearchAddAPI {
 	@GET
 	@Produces("application/xml")
 	public String solrApi() {
+//		writeJavaBinToSolr();
+
+		writeXMLToSolr();
+		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
+	}
+
+	private void writeXMLToSolr() {
+
+		ResultSet rs =  getResultSets();
+		try {
+			Document doc = toDocument(rs);
+			System.out.println(doc);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void writeJavaBinToSolr() {
+		
+		ResultSet rs =  getResultSets();
+		try {
+			DBAccessor accessor = new DBAccessor();
+			//accessor.addResultSet(rs);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Path("{f}")
+	@GET
+	@Produces("application/xml")
+	public String solrApi(@PathParam("f") Double f) {
+		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
+	}
+
+	private ResultSet getResultSets() {
 		Statement stmt = null;
 		String query = "select ce.catalog_entry_id, ce.entry_type, ce.short_description, ce.long_description,ce.title "
 				+ " from catalog_entry ce, store_product sp "
@@ -36,42 +83,43 @@ public class SolrSearchAddAPI {
 
 			stmt = connection.createStatement();
 			rs = stmt.executeQuery(query);
-
-	        if (rs != null) {
-	            try {
-	                DBAccessor accessor = new DBAccessor();
-	                accessor.addResultSet(rs);
-	            } catch (SQLException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-	            } catch (SolrServerException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-	            } catch (IOException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-	            }
-	        }
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		} finally {
-			if (connection != null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
+		}
+		
+		return rs;
+	}
+
+	public static Document toDocument(ResultSet rs)
+			throws ParserConfigurationException, SQLException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document doc = builder.newDocument();
+
+		Element results = doc.createElement("Results");
+		doc.appendChild(results);
+
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int colCount = rsmd.getColumnCount();
+
+		while (rs.next()) {
+			Element row = doc.createElement("Row");
+			results.appendChild(row);
+
+			for (int i = 1; i <= colCount; i++) {
+				String columnName = rsmd.getColumnName(i);
+				Object value = rs.getObject(i);
+				
+				if(value != null ) {
+					System.out.println(" ColumnName : " + columnName + " Value : " + value );
+					Element node = doc.createElement(columnName);
+					node.appendChild(doc.createTextNode(value.toString()));
+					row.appendChild(node);
 				}
 			}
 		}
-		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
-	}
-
-	@Path("{f}")
-	@GET
-	@Produces("application/xml")
-	public String solrApi(@PathParam("f") Double f) {
-		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
+		return doc;
 	}
 }
