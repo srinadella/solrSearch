aba1a459b0913bfffc67da71597dddd34f4807d2
cnadella@gmail.com <cnadella@gmail.com>
2016-03-07 22:42:25 -0500

    Fix Solr Call Method

--- src/main/java/net/search/solr/services/SolrSearchAddAPI.java
+++ src/main/java/net/search/solr/services/SolrSearchAddAPI.java
@@ -5,7 +5,6 @@ import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -13,113 +12,67 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.solr.client.solrj.SolrServerException;
 
 import net.search.solr.db.DBAccessor;
 
-import org.apache.solr.client.solrj.SolrServerException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
 @Path("/solrAddApi")
 public class SolrSearchAddAPI {
-	@GET
-	@Produces("application/xml")
-	public String solrApi() {
-//		writeJavaBinToSolr();
+    @GET
+    @Produces("application/xml")
+    public String solrApi() {
+        writeJavaBinToSolr();
+        return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
+    }
 
-		writeXMLToSolr();
-		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
-	}
+    private void writeJavaBinToSolr() {
+        try {
+            ResultSet rs = getResultSets();
+            DBAccessor accessor = new DBAccessor();
+            accessor.addResultSet(rs);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void writeXMLToSolr() {
+    @Path("{f}")
+    @GET
+    @Produces("application/xml")
+    public String solrApi(@PathParam("f") Double f) {
+        return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
+    }
 
-		ResultSet rs =  getResultSets();
-		try {
-			Document doc = toDocument(rs);
-			System.out.println(doc);
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+    private ResultSet getResultSets() {
+        Statement stmt = null;
+        String query =
+                "select ce.catalog_entry_id, ce.entry_type, ce.short_description, ce.long_description,ce.title " +
+                        " from catalog_entry ce, store_product sp " +
+                        " where sp.store_code='TRUS' and sp.sp_status like 'A%' and sp.product_id = ce.catalog_entry_id and ce.status like 'A%' and" +
+                        " rownum < 10";
+        Connection connection = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@lvsdtumerdb01a.gspt.net:1521:mercaldv1",
+                    "NADELLAS", "cOmmerce1");
 
-	private void writeJavaBinToSolr() {
-		
-		ResultSet rs =  getResultSets();
-		try {
-			DBAccessor accessor = new DBAccessor();
-			//accessor.addResultSet(rs);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-	@Path("{f}")
-	@GET
-	@Produces("application/xml")
-	public String solrApi(@PathParam("f") Double f) {
-		return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
-	}
+        return rs;
+    }
 
-	private ResultSet getResultSets() {
-		Statement stmt = null;
-		String query = "select ce.catalog_entry_id, ce.entry_type, ce.short_description, ce.long_description,ce.title "
-				+ " from catalog_entry ce, store_product sp "
-				+ " where sp.store_code='TRUS' and sp.sp_status like 'A%' and sp.product_id = ce.catalog_entry_id and ce.status like 'A%' and"
-				+ " rownum < 10";
-		Connection connection = null;
-		ResultSet rs = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			connection = DriverManager.getConnection(
-					"jdbc:oracle:thin:@lvsdtumerdb01a.gspt.net:1521:mercaldv1",
-					"NADELLAS", "cOmmerce1");
-
-			stmt = connection.createStatement();
-			rs = stmt.executeQuery(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		return rs;
-	}
-
-	public static Document toDocument(ResultSet rs)
-			throws ParserConfigurationException, SQLException {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		Document doc = builder.newDocument();
-
-		Element results = doc.createElement("Results");
-		doc.appendChild(results);
-
-		ResultSetMetaData rsmd = rs.getMetaData();
-		int colCount = rsmd.getColumnCount();
-
-		while (rs.next()) {
-			Element row = doc.createElement("Row");
-			results.appendChild(row);
-
-			for (int i = 1; i <= colCount; i++) {
-				String columnName = rsmd.getColumnName(i);
-				Object value = rs.getObject(i);
-				
-				if(value != null ) {
-					System.out.println(" ColumnName : " + columnName + " Value : " + value );
-					Element node = doc.createElement(columnName);
-					node.appendChild(doc.createTextNode(value.toString()));
-					row.appendChild(node);
-				}
-			}
-		}
-		return doc;
-	}
 }
