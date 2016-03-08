9a22aa53478556194755c180929f12f77d1f17f3
cnadella@gmail.com <cnadella@gmail.com>
2016-03-08 10:33:48 -0500

    Server Code Updates

--- src/main/java/net/search/solr/services/SolrSearchAddAPI.java
+++ src/main/java/net/search/solr/services/SolrSearchAddAPI.java
@@ -29,8 +29,39 @@ public class SolrSearchAddAPI {
     @GET
     @Produces("application/xml")
     public String solrApi() {
-        writeJavaBinToSolr();
+        writeJavaBinToSolr(10);
 
+        StringBuffer response = buildResponse();
+        return response.toString();
+    }
+    @Path("{num}")
+    @GET
+    @Produces("application/xml")
+    public String solrApi(@PathParam("num") int num) {
+        writeJavaBinToSolr(num);
+
+        StringBuffer response = buildResponse();
+        return response.toString();
+    }
+
+    private void writeJavaBinToSolr(int num) {
+        try {
+            ResultSet rs = getResultSets(num);
+            DBAccessor accessor = new DBAccessor();
+            accessor.addResultSet(rs);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private StringBuffer buildResponse() {
         List<Product> products;
         StringBuffer response = new StringBuffer();
         response.append("<solrAddApi>"); 
@@ -49,23 +80,7 @@ public class SolrSearchAddAPI {
         } catch (SolrServerException | IOException e) {
             e.printStackTrace();
         }
-        return response.toString();
-    }
-
-    private void writeJavaBinToSolr() {
-        try {
-            ResultSet rs = getResultSets();
-            DBAccessor accessor = new DBAccessor();
-            accessor.addResultSet(rs);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (SolrServerException e) {
-            e.printStackTrace();
-        }
+        return response;
     }
 
     private static List<Product> querySimpleSolr() throws SolrServerException, IOException {
@@ -89,20 +104,13 @@ public class SolrSearchAddAPI {
         return beans;
     }
 
-    @Path("{f}")
-    @GET
-    @Produces("application/xml")
-    public String solrApi(@PathParam("f") Double f) {
-        return "<solrAddApi> <fahrenheit></fahrenheit><ftocoutput> </ftocoutput></solrAddApi>";
-    }
-
-    private ResultSet getResultSets() {
+    private ResultSet getResultSets(int num) {
         Statement stmt = null;
         String query =
                 "select ce.catalog_entry_id, ce.entry_type, ce.short_description, ce.long_description,ce.title " +
                         " from catalog_entry ce, store_product sp " +
                         " where sp.store_code='TRUS' and sp.sp_status like 'A%' and sp.product_id = ce.catalog_entry_id and ce.status like 'A%' and" +
-                        " rownum < 10";
+                        " rownum < " + num;
         Connection connection = null;
         ResultSet rs = null;
         try {
